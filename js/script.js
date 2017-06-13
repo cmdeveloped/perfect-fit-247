f43d02c407d72ae37a3cd81e6ddc80e66cbcf378
cmdeveloped <cmwebdevs@gmail.com>
2017-06-12 21:06:28 -0500

    Added top banner that shows the hours for current day using javascript.

---
+++ js/script.js
@@ -0,0 +1,33 @@
+$(document).ready(function() {
+
+  switch (new Date().getDay()) {
+      case 0:
+          text = "By Appointment Only";
+          break;
+      case 1:
+          text = "Noon - 7PM";
+          break;
+      case 2:
+          text = "Noon - 7PM";
+          break;
+      case 3:
+          text = "10AM - 5PM";
+          break;
+      case 4:
+          text = "By Appointment Only";
+          break;
+      case 5:
+          text = "10AM - 5PM";
+          break;
+      case 6:
+          text = "11AM - 1PM";
+          break;
+      default:
+          text = "hello";
+          break;
+  }
+
+  document.getElementById("message").innerHTML = 'Today&#39s Hours: ';
+  document.getElementById("hours").innerHTML = text;
+
+});
